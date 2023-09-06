45291482565553769af43dd14549ab3dea846843
Psaw19 <prashantsaw19@gmail.com>
2023-09-06 12:16:03 +0530

    updated cors origin

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "http://localhost:3000" || "https://studynotion-backend-zmow.onrender.com",
         credentials: true,
     })
 )
