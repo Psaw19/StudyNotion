cf058ae5657e683d091e4c61dd207460e2787f3c
Psaw19 <prashantsaw19@gmail.com>
2023-11-22 02:16:01 +0530

    update readme

--- src/components/common/IconBtn.jsx
+++ src/components/common/IconBtn.jsx
@@ -5,7 +5,7 @@ const IconBtn = ({ children, onclick, active, type }) => {
         <button
             type={type}
             onClick={onclick}
-            className={`${active ? 'bg-yellow-50' : 'bg-richblack-400'} px-5 py-2 rounded text-richblack-900 text-base font-medium`}>
+            className={`${active ? 'bg-yellow-50' : 'bg-richblack-400'} px-5 py-2 rounded text-richblack-900 text-base flex items-center justify-center gap-1 font-medium`}>
             {children}
         </button>
     )
--- src/components/core/Dashboard/MyProfile.jsx
+++ src/components/core/Dashboard/MyProfile.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import IconBtn from "../../common/IconBtn";
+import { TbEdit } from "react-icons/tb";
 
 const MyProfile = () => {
-    return (
-        <div className=''>MyProfile</div>
-    )
-}
+  const { user } = useSelector((state) => state.profile);
+  const navigate = useNavigate();
 
-export default MyProfile
+  return (
+    <>
+      <div className="">MyProfile</div>
+
+      {/* section1 */}
+      <div className="flex items-center justify-between w-5/6 p-8 mx-auto border rounded-lg bg-richblack-800 border-richblack-700">
+        <div className="flex items-center gap-4">
+          <img src={user?.image} alt="dp" className="w-20 rounded-full" />
+          <div>
+            <p className="text-xl font-semibold">
+              {user?.firstName} {user?.lastName}
+            </p>
+            <p className="text-richblack-300">{user?.email}</p>
+          </div>
+        </div>
+
+        <IconBtn active={true} onclick={() => navigate("/dashboard/settings")}>
+          <TbEdit size={20} />
+          Edit
+        </IconBtn>
+      </div>
+    </>
+  );
+};
+
+export default MyProfile;
